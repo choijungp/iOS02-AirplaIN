ffd56f0a56f34ed79749cadab651acdfe21a2c46
최정인 <wjddls980912@naver.com>
2024-10-30 17:02:11 +0900

    chore: 프로젝트 생성

---
+++ AirplaIN/AirplaIN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirplaIN
+//
+//  Created by 최정인 on 10/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
